57e6853a99ff33d8848c2968093553bd8a445686
Wu Haotian <whtsky@gmail.com>
2022-04-07 21:41:51 +0800

    test: unify tests

--- tests/test_maho.py
+++ tests/test_maho.py
@@ -1,3 +1,5 @@
+from typing import Optional
+
 import pytest
 
 from mypy_silent.maho import add_type_ignore_comment
@@ -5,23 +7,30 @@ from mypy_silent.maho import remove_type_ignore_comment
 
 
 @pytest.mark.parametrize(
-    "input,output",
+    "input,error_code,output",
     (
         (
             "        host, port, protocol = m.groups()\r\n",
+            None,
             "        host, port, protocol = m.groups()  # type: ignore\r\n",
         ),
-        ("print(a(2, 's'))  # noqa", "print(a(2, 's'))  # type: ignore # noqa"),
+        (
+            "        host, port, protocol = m.groups()\r\n",
+            "misc",
+            "        host, port, protocol = m.groups()  # type: ignore[misc]\r\n",
+        ),
+        ("print(a(2, 's'))  # noqa", None, "print(a(2, 's'))  # type: ignore # noqa"),
+        (
+            "print(a(2, 's'))  # noqa",
+            "misc",
+            "print(a(2, 's'))  # type: ignore[misc] # noqa",
+        ),
     ),
 )
-def test_add_type_ignore_comment(input: str, output: str) -> None:
-    assert add_type_ignore_comment(input, error_code=None) == output
-
-
-def test_add_type_ignore_comment_with_error_code() -> None:
-    input = "host, port, protocol = m.groups()\r\n"
-    output = "host, port, protocol = m.groups()  # type: ignore[misc]\r\n"
-    assert add_type_ignore_comment(input, error_code="misc") == output
+def test_add_type_ignore_comment(
+    input: str, error_code: Optional[str], output: str
+) -> None:
+    assert add_type_ignore_comment(input, error_code=error_code) == output
 
 
 @pytest.mark.parametrize(
@@ -29,6 +38,10 @@ def test_add_type_ignore_comment_with_error_code() -> None:
     (
         (
             "        host, port, protocol = m.groups()  # type: ignore\r\n",
+            "        host, port, protocol = m.groups()\r\n",
+        ),
+        (
+            "        host, port, protocol = m.groups()  # type: ignore[misc]\r\n",
             "        host, port, protocol = m.groups()\r\n",
         ),
         ("# type: ignore. very good", "#. very good"),
